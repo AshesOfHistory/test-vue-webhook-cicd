88e1b37d17f5824076ab8ee3cd751eab0dfdd301
王鹏 <wangpeng_f@aspirecn.com>
2021-01-30 00:43:42 +0800

    11

--- webhook.js
+++ webhook.js
@@ -6,7 +6,7 @@ function sign(body) {
   return `sha1=${crypto.createHmac('sha1', SECRET).update(body).digest('hex')}`
 }
 let server = http.createServer(function(req, res) {
-  console.log(req.method, req.url, req)
+  console.log(req.method, req.url)
   if (req.method == 'POST' && req.url == '/test-vue-webhook-cicd') {
 
     let buffers = []
@@ -15,11 +15,10 @@ let server = http.createServer(function(req, res) {
       buffers.push(buffer)
     })
     req.on('end', function(buffer) {
-      let body = buffers.concat(buffer)
-      console.log('req: ', req)
+      let body = Buffer.concat(buffers)
       let event = req.headers['x-github-event'] // event = push
       // github 请求来的时候 要传递请求题body  另外还会传一个签名过来 signature，你需要本地校验签名是否正确
-      let signature = req.headers['x-gihub-signature']
+      let signature = req.headers['x-hub-signature']
       if (signature !== sign(body)) {
         res.end('Not allowed')
       } else {
@@ -29,7 +28,15 @@ let server = http.createServer(function(req, res) {
         if (event == 'push') { // 开始部署
           let payload = JSON.parse(body)
           console.log('payload', payload)
-          // spawn('sh', [`./${payload.repository.name}`])
+          let child = spawn('sh', [`./${payload.repository.name}.sh`])
+          let buffers = []
+          child.stdout.on('data', function(buffer) {
+            buffers.push(buffer)
+          })
+          child.stdout.on('end', function(buffer) {
+            let log = Buffer.concat(buffers).toString()
+            console.log(log)
+          })
         }
       }
     })
